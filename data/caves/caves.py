1e22ead59a145e3e938265d334228c7f67f61113
mgoulish <mgoulish@redhat.com>
2022-02-08 12:54:40 -0500

    Python tool to create tif image from Mars Caves Catalog database csv file.

---
+++ data/caves/caves.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+
+from PIL import Image
+
+width  = 3600
+height = 1800
+
+img = Image.new ( mode = "RGB",
+                  size = (width, height),
+                  color = ( 0, 0, 0 ) )
+
+pixels = img.load ( )
+
+# Get the data!
+fp = open ( 'data.csv', 'r' )
+lines = fp.readlines()
+
+for line in lines :
+  words = line.split ( ',' ) 
+  long  = (1800 + int(float(words[1]) * 10.0)) % 3600
+  lat   = 180 - int((float(words[2])+90.0) * 10)
+  pixels [ long, lat ] = ( 255, 255, 255 )
+
+img.save ( "img.tif" )
+
